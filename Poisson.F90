b549bbd8a637e9a163bdd5cf31d6a178edfbeeb4
Juhani Kataja <juhani.kataja@csc.fi>
2024-04-22 16:24:14 +0300

    elementmetricgpu

--- Poisson.F90
+++ Poisson.F90
@@ -1,5 +1,5 @@
 module mylinearforms
-  USE Types, only: dp, VECTOR_BLOCK_LENGTH, VECTOR_SMALL_THRESH
+  USE Types, only: dp, VECTOR_BLOCK_LENGTH, VECTOR_SMALL_THRESH, Element_t, Nodes_t
 
   !INTEGER, PARAMETER :: dp = SELECTED_REAL_KIND(12) 
   !INTEGER, PARAMETER :: VECTOR_BLOCK_LENGTH = 128                                                                                                                                                                                                                                                                                                      
@@ -7,10 +7,278 @@ module mylinearforms
 
   PUBLIC :: LinearForms_GradUdotGradU
   
-   !!$omp declare target (LinearForms_GradUdotGradU)
 
    !$omp declare target to(LinearForms_GradUdotGradU)
 contains
+
+   FUNCTION ElementMetricGPU( Elm, Nodes, nc, ndof, DetJ, nbmax, dLBasisdx, LtoGMap) RESULT(AllSuccess)
+!------------------------------------------------------------------------------
+    !$OMP DECLARE TARGET
+     TYPE(Element_t)  :: Elm                                 !< Element structure
+     TYPE(Nodes_t)    :: Nodes                               !< element nodal coordinates
+     INTEGER, INTENT(IN) :: nc                               !< Number of points to map
+     INTEGER :: ndof                                         !< Number of active nodes in element
+     REAL(KIND=dp) :: DetJ(VECTOR_BLOCK_LENGTH)              !< SQRT of determinant of element coordinate metric at each point
+     INTEGER, INTENT(IN) :: nbmax                            !< Maximum total number of basis functions in local basis
+     REAL(KIND=dp) :: dLBasisdx(VECTOR_BLOCK_LENGTH,nbmax,3) !< Derivatives of element basis function with 
+                                                             !<  respect to local coordinates at each point
+     REAL(KIND=dp) :: LtoGMap(VECTOR_BLOCK_LENGTH,3,3)       !< Mapping between local and global coordinates
+     LOGICAL :: AllSuccess                  !< Returns .FALSE. if some point in element is degenerate
+!------------------------------------------------------------------------------
+!       Local variables
+!------------------------------------------------------------------------------
+     REAL(KIND=dp) :: dx(VECTOR_BLOCK_LENGTH,3,3)
+     REAL(KIND=dp) :: Metric(VECTOR_BLOCK_LENGTH,6), &
+             G(VECTOR_BLOCK_LENGTH,6)       ! Symmetric Metric(nc,3,3) and G(nc,3,3)
+
+     REAL(KIND=dp) :: s
+     INTEGER :: cdim,dim,i,j,k,l,n,ip, jj, kk
+     INTEGER :: ldbasis, ldxyz, utind
+     !------------------------------------------------------------------------------
+     AllSuccess = .TRUE.
+
+     ! Coordinates (single array)
+     n = MIN( SIZE(Nodes % x, 1), ndof )
+
+     ! Dimensions (coordinate system and element)
+     cdim = CoordinateSystemDimension()
+     dim  = elm % TYPE % DIMENSION
+
+     ! Leading dimensions for local basis and coordinate arrays
+     ldbasis = SIZE(dLBasisdx, 1)
+     ldxyz = SIZE(Nodes % xyz, 1)
+
+     ! For linear, extruded and otherwise regular elements mapping has to be computed
+     ! only once, the problem is to identify these cases...
+     !------------------------------------------------------------------------------
+     !       Partial derivatives of global coordinates with respect to local coordinates
+     !------------------------------------------------------------------------------
+     ! Avoid DGEMM calls for nc small
+     !IF (nc < VECTOR_SMALL_THRESH) THEN
+       DO l=1,dim
+         DO j=1,3
+           dx(1:nc,j,l)=REAL(0,dp)
+           DO k=1,n
+!DIR$ UNROLL
+             DO i=1,nc
+               dx(i,j,l)=dx(i,j,l)+dLBasisdx(i,k,l)*Nodes % xyz(k,j)
+             END DO
+           END DO
+         END DO
+       END DO
+       
+     !ELSE ! Here would be some call to gpu aware DGEMM
+       !DO i=1,dim
+         !CALL DGEMM('N','N',nc, 3, n, &
+                 !REAL(1,dp), dLbasisdx(1,1,i), ldbasis, &
+                 !Nodes % xyz, ldxyz, REAL(0, dp), dx(1,1,i), VECTOR_BLOCK_LENGTH)
+       !END DO
+     !END IF
+
+     !------------------------------------------------------------------------------
+     !       Compute the covariant metric tensor of the element coordinate system (symmetric)
+     !------------------------------------------------------------------------------
+     ! Linearized upper triangular indices for accesses to G
+     ! | (1,1) (1,2) (1,3) | = | 1 2 4 |
+     ! |       (2,2) (2,3) |   |   3 5 |
+     ! |             (3,3) |   |     6 |
+     ! G is symmetric, compute only the upper triangular part of G=dx^Tdx
+     DO j=1,dim
+       DO i=1,j
+         utind = GetSymmetricIndex(i,j)
+         SELECT CASE (cdim)
+         CASE(1)
+           !_ELMER_OMP_SIMD
+           DO l=1,nc
+             G(l,utind)=dx(l,1,i)*dx(l,1,j)
+           END DO
+         CASE(2)
+           !_ELMER_OMP_SIMD
+           DO l=1,nc
+             G(l,utind)=dx(l,1,i)*dx(l,1,j)+dx(l,2,i)*dx(l,2,j)
+           END DO
+         CASE(3)
+           !_ELMER_OMP_SIMD
+           DO l=1,nc
+             G(l,utind)=dx(l,1,i)*dx(l,1,j)+dx(l,2,i)*dx(l,2,j)+dx(l,3,i)*dx(l,3,j)
+           END DO
+         END SELECT
+       END DO
+     END DO
+
+     !------------------------------------------------------------------------------
+     !       Convert the metric to contravariant base, and compute the SQRT(DetG)
+     !------------------------------------------------------------------------------
+     SELECT CASE( dim )
+       !------------------------------------------------------------------------------
+       !       Line elements
+       !------------------------------------------------------------------------------
+     CASE (1)
+       ! Determinants
+       ! DetJ(1:nc)  = G(1:nc,1,1)
+       DetJ(1:nc)  = G(1:nc,1)
+
+       DO i=1,nc
+         IF (DetJ(i) <= TINY(REAL(1,dp))) THEN
+           AllSuccess = .FALSE.
+           EXIT
+         END IF
+       END DO
+
+       IF (AllSuccess) THEN
+         !_ELMER_OMP_SIMD
+         DO i=1,nc
+           ! Metric(i,1,1) = REAL(1,dp)/DetJ(i)
+           Metric(i,1) = REAL(1,dp)/DetJ(i)
+         END DO
+         !_ELMER_OMP_SIMD
+         DO i=1,nc
+           DetJ(i) = SQRT( DetJ(i))
+         END DO
+       END IF
+
+
+       !------------------------------------------------------------------------------
+       !       Surface elements
+       !------------------------------------------------------------------------------
+     CASE (2)
+       ! Determinants
+       !_ELMER_OMP_SIMD
+       DO i=1,nc
+         ! DetJ(i) = ( G(i,1,1)*G(i,2,2) - G(i,1,2)*G(i,2,1) )
+         ! G is symmetric
+         DetJ(i) = G(i,1)*G(i,3)-G(i,2)*G(i,2)
+       END DO
+
+       DO i=1,nc
+         IF (DetJ(i) <= TINY(REAL(1,dp))) THEN
+           AllSuccess = .FALSE.
+           EXIT
+         END IF
+       END DO
+
+       IF (AllSuccess) THEN
+         ! Since G=G^T, it holds G^{-1}=(G^T)^{-1}
+         !_ELMER_OMP_SIMD
+         DO i=1,nc
+           s = REAL(1,dp)/DetJ(i)
+           ! G is symmetric
+           ! All in one go, with redundancies eliminated
+           Metric(i,1) =  s*G(i,3)
+           Metric(i,2) = -s*G(i,2)
+           Metric(i,3) =  s*G(i,1)
+         END DO
+         !_ELMER_OMP_SIMD
+         DO i=1,nc
+           DetJ(i) = SQRT(DetJ(i))
+         END DO
+
+       END IF
+       !------------------------------------------------------------------------------
+       !       Volume elements
+       !------------------------------------------------------------------------------
+     CASE (3)
+       ! Determinants
+       !_ELMER_OMP_SIMD
+       DO i=1,nc
+         ! DetJ(i) = G(i,1,1) * ( G(i,2,2)*G(i,3,3) - G(i,2,3)*G(i,3,2) ) + &
+         !           G(i,1,2) * ( G(i,2,3)*G(i,3,1) - G(i,2,1)*G(i,3,3) ) + &
+         !           G(i,1,3) * ( G(i,2,1)*G(i,3,2) - G(i,2,2)*G(i,3,1) )
+         ! G is symmetric
+         DetJ(i) = G(i,1)*(G(i,3)*G(i,6)-G(i,5)*G(i,5)) + &
+                 G(i,2)*(G(i,5)*G(i,4)-G(i,2)*G(i,6)) + &
+                 G(i,4)*(G(i,2)*G(i,5)-G(i,3)*G(i,4))
+       END DO
+
+       DO i=1,nc
+         IF (DetJ(i) <= TINY(REAL(1,dp))) THEN
+           AllSuccess = .FALSE.
+           EXIT
+         END IF
+       END DO
+
+       IF (AllSuccess) THEN
+         ! Since G=G^T, it holds G^{-1}=(G^T)^{-1}
+         !_ELMER_OMP_SIMD
+         DO i=1,nc
+           s = REAL(1,dp) / DetJ(i)
+           ! Metric(i,1,1) =  s * (G(i,2,2)*G(i,3,3) - G(i,3,2)*G(i,2,3))
+           ! Metric(i,2,1) = -s * (G(i,2,1)*G(i,3,3) - G(i,3,1)*G(i,2,3))
+           ! Metric(i,3,1) =  s * (G(i,2,1)*G(i,3,2) - G(i,3,1)*G(i,2,2))
+           ! G is symmetric
+
+           ! All in one go, with redundancies eliminated
+           Metric(i,1)= s*(G(i,3)*G(i,6)-G(i,5)*G(i,5))
+           Metric(i,2)=-s*(G(i,2)*G(i,6)-G(i,4)*G(i,5))
+           Metric(i,3)= s*(G(i,1)*G(i,6)-G(i,4)*G(i,4))
+           Metric(i,4)= s*(G(i,2)*G(i,5)-G(i,3)*G(i,4))
+           Metric(i,5)=-s*(G(i,1)*G(i,5)-G(i,2)*G(i,4))
+           Metric(i,6)= s*(G(i,1)*G(i,3)-G(i,2)*G(i,2))
+         END DO
+
+         !_ELMER_OMP_SIMD
+         DO i=1,nc
+           DetJ(i) = SQRT(DetJ(i))
+         END DO
+
+       END IF
+     END SELECT
+
+     IF (AllSuccess) THEN
+       SELECT CASE(dim)
+       CASE(1)
+!DIR$ LOOP COUNT MAX=3
+         DO i=1,cdim
+           !_ELMER_OMP_SIMD
+           DO l=1,nc
+             LtoGMap(l,i,1) = dx(l,i,1)*Metric(l,1)
+           END DO
+         END DO
+       CASE(2)
+!DIR$ LOOP COUNT MAX=3
+         DO i=1,cdim
+           !_ELMER_OMP_SIMD
+           DO l=1,nc
+             LtoGMap(l,i,1) = dx(l,i,1)*Metric(l,1) + dx(l,i,2)*Metric(l,2)
+             LtoGMap(l,i,2) = dx(l,i,1)*Metric(l,2) + dx(l,i,2)*Metric(l,3)
+           END DO
+         END DO
+       CASE(3)
+!DIR$ LOOP COUNT MAX=3
+         DO i=1,cdim
+           !_ELMER_OMP_SIMD
+           DO l=1,nc
+             LtoGMap(l,i,1) = dx(l,i,1)*Metric(l,1) + dx(l,i,2)*Metric(l,2) + dx(l,i,3)*Metric(l,4)
+             LtoGMap(l,i,2) = dx(l,i,1)*Metric(l,2) + dx(l,i,2)*Metric(l,3) + dx(l,i,3)*Metric(l,5)
+             LtoGMap(l,i,3) = dx(l,i,1)*Metric(l,4) + dx(l,i,2)*Metric(l,5) + dx(l,i,3)*Metric(l,6)
+           END DO
+         END DO
+       END SELECT
+     ELSE
+
+       ! Degenerate element!
+       ! return some error
+       ! TODO: assuming non-degenerate elements! 
+     END IF
+
+   CONTAINS
+
+     PURE FUNCTION GetSymmetricIndex(i,j) RESULT(utind)
+       IMPLICIT NONE
+       !$OMP DECLARE TARGET
+       INTEGER, INTENT(IN) :: i, j
+       INTEGER :: utind
+
+       IF (i>j) THEN
+         utind = i*(i-1)/2+j
+       ELSE
+         utind = j*(j-1)/2+i
+       END IF
+     END FUNCTION GetSymmetricIndex
+!------------------------------------------------------------------------------
+   END FUNCTION ElementMetricGPU
+
+
   SUBROUTINE LinearForms_GradUdotGradU(m, n, dim, GradU, weight, G, alpha)
     implicit none
     INTEGER, INTENT(IN) :: m, n, dim
@@ -75,6 +343,7 @@ contains
       END IF
     END DO ! Vector blocks
   END SUBROUTINE LinearForms_GradUdotGradU
+
 end module mylinearforms
 
 !-----------------------------------------------------------------------------
@@ -131,6 +400,7 @@ SUBROUTINE AdvDiffSolver( Model,Solver,dt,TransientSimulation )
 
   type :: elem_ptr
     type(Element_t), pointer :: p
+    type(Nodes_t), pointer :: nodes
     integer :: n, nd, nb                        ! nof nodes, nof dofs, nofbdofs
   end type
 
@@ -189,27 +459,28 @@ SUBROUTINE AdvDiffSolver( Model,Solver,dt,TransientSimulation )
 
   !! Tabulate elements and their ndofs/nnodes/nb 
   nColours = GetNOFColours(Solver)
-  !!$OMP PARALLEL &
-  !!$OMP SHARED(Active, Solver, nColours, VecAsm, elem_lists) &
-  !!$OMP PRIVATE(t, Element, n, nd, nb, col, InitHandles) & 
-  !!$OMP REDUCTION(max:MaxNumNodes) DEFAULT(none)
+  !$OMP PARALLEL &
+  !$OMP SHARED(Active, Solver, nColours, VecAsm, elem_lists) &
+  !$OMP PRIVATE(t, Element, n, nd, nb, col, InitHandles) & 
+  !$OMP REDUCTION(max:MaxNumNodes) DEFAULT(none)
   do col=1,ncolours
-    !!$OMP SINGLE
+    !$OMP SINGLE
     active = GetNOFActive(Solver)
-    !!$OMP END SINGLE
+    !$OMP END SINGLE
 
-    !!$OMP DO
+    !$OMP DO
     do t=1,active
       Element => GetActiveElement(t)
       elem_lists(col) % elements(t) % p => Element
       elem_lists(col) % elements(t) % n = GetElementNOFNodes(Element)
       elem_lists(col) % elements(t) % nd = GetElementNOFDOFs(Element)
       elem_lists(col) % elements(t) % nb = GetElementNOFBDOFs(Element)
+      CALL GetElementNodesVec( elem_lists(col) % elements(t) % nodes,  elem_lists(col) % elements(t) % p)
       MaxNumNodes = max(MaxNumNodes,elem_lists(col) % elements(t) % n)
     end do
-    !!$OMP END DO
+    !$OMP END DO
   end do
-  !!$OMP END PARALLEL
+  !$OMP END PARALLEL
 
   CALL CheckTimer(Caller//'BulkAssembly', Delete=.TRUE.)
 
@@ -226,7 +497,9 @@ SUBROUTINE AdvDiffSolver( Model,Solver,dt,TransientSimulation )
       n = elem_lists(col) % elements(t) % n
       nd = elem_lists(col) % elements(t) % nd
       nb = elem_lists(col) % elements(t) % nb
-      CALL LocalMatrixVec(  Element, n, nd+nb, nb, VecAsm )
+      !!$omp target
+      CALL LocalMatrixVec(  Element, n, nd+nb, nb, VecAsm, elem_lists(col)% elements(t) % nodes)
+      !!$omp end target
     END DO
   END DO
 
@@ -289,19 +562,21 @@ CONTAINS
 
 ! Assembly of the matrix entries arising from the bulk elements. SIMD version.
 !------------------------------------------------------------------------------
-  SUBROUTINE LocalMatrixVec( Element, n, nd, nb, VecAsm )
+  SUBROUTINE LocalMatrixVec( Element, n, nd, nb, VecAsm, Nodes)
 !------------------------------------------------------------------------------
-    USE LinearForms
-    !use mylinearforms
+    !USE LinearForms
+    use MyLinearforms
     USE Integration
     use iso_c_binding
-
-
+    use Types
     IMPLICIT NONE
+  !$omp declare target to(LocalMatrixVec)
+
+
     INTEGER, INTENT(IN) :: n, nd, nb
     TYPE(Element_t), POINTER:: Element
     LOGICAL, INTENT(IN) :: VecAsm
-    TYPE(element_t) :: concrete_element
+    TYPE(Nodes_t), POINTER :: Nodes
 !------------------------------------------------------------------------------
     REAL(KIND=dp), ALLOCATABLE, SAVE :: Basis(:,:),dBasisdx(:,:,:), DetJ(:)
     REAL(KIND=dp), ALLOCATABLE, SAVE :: MASS(:,:), STIFF(:,:), FORCE(:)
@@ -311,8 +586,8 @@ CONTAINS
     LOGICAL :: Stat,Found
     INTEGER :: i,t,p,q,dim,ngp,allocstat
     TYPE(GaussIntegrationPoints_t) :: IP
-    TYPE(Nodes_t), SAVE :: Nodes
     LOGICAL, SAVE :: FirstTime=.TRUE.
+    TYPE(element_t) :: concrete_element
 
     !!$OMP THREADPRIVATE(Basis, dBasisdx, DetJ, &
     !!$OMP               MASS, STIFF, FORCE, Nodes, &
@@ -351,7 +626,8 @@ CONTAINS
     END IF
 
 
-    CALL GetElementNodesVec( Nodes, UElement=Element )
+    ! Do this on cpu
+    !CALL GetElementNodesVec( Nodes, UElement=Element )
 
     ! Initialize
     MASS  = 0._dp
